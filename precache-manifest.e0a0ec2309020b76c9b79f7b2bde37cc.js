2ea649971f250d780795d244738b830fa40e2535
aayushi kambariya <aayushi.kambriya5@gmail.com>
2020-06-13 23:46:50 +0530

    Updates

---
+++ precache-manifest.e0a0ec2309020b76c9b79f7b2bde37cc.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c19a47d7be7063fb90dcbb24f2eb1668",
+    "url": "/Todo-React-App/index.html"
+  },
+  {
+    "revision": "5333248ec2abde21aec9",
+    "url": "/Todo-React-App/static/css/main.38759f63.chunk.css"
+  },
+  {
+    "revision": "8394a3940453b3747d7d",
+    "url": "/Todo-React-App/static/js/2.5158276e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Todo-React-App/static/js/2.5158276e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5333248ec2abde21aec9",
+    "url": "/Todo-React-App/static/js/main.59d1d58d.chunk.js"
+  },
+  {
+    "revision": "1bf9d76f20634ee3fe68",
+    "url": "/Todo-React-App/static/js/runtime-main.2b7c98bd.js"
+  },
+  {
+    "revision": "47cd84b49ff28da6c47768d6ac1d2cd5",
+    "url": "/Todo-React-App/static/media/delete.47cd84b4.svg"
+  },
+  {
+    "revision": "de320fab86422efbd0d239fdf0509a9e",
+    "url": "/Todo-React-App/static/media/edit.de320fab.svg"
+  }
+]);
